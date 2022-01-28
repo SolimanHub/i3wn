771fc4123d07f4b95d70f963bedffffb01ef7c33
solimanhub <david.soliman2010@gmail.com>
2022-01-28 13:57:24 -0600

    Mods de forma

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 mkdir -p $HOME/.config/polybar/
 cp /usr/share/doc/polybar/config $HOME/.config/polybar/
-mv launch.sh $HOME/.config/polybar/
+cp launch.sh $HOME/.config/polybar/
 cd $HOME/.config/polybar/
 source launch.sh
