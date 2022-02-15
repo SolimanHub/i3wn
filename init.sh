fd6fcb95db60766412c078f66bcbfb88e7324b83
solimanhub <david.soliman2010@gmail.com>
2022-02-15 02:26:05 -0600

    scripts & wallpapers

--- init.sh
+++ init.sh
@@ -3,3 +3,10 @@
 mkdir -p $HOME/.config/polybar/
 ln -s $HOME/.config/i3/polybar $HOME/.config/polybar
 source $HOME/.config/polybar/launch.sh
+sudo chmod 777 -R /opt/
+cd /opt/
+git clone git@github.com:SolimanHub/scripts.git
+sudo ln -s /opt/scripts/scregcp /bin/scregcp
+sudo ln -s /opt/scripts/wallpaper /bin/wallpaper
+cd $HOME/Imágenes/
+git clone git@github.com:SolimanHub/wallpapers.git 
